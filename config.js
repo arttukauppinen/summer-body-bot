00910fd84d6d7e07fde1965c1fe947b26ac2e7a9
arttukauppinen <kauppinen27@gmail.com>
2024-03-12 10:40:32 +0200

    more logic for retrieving env variables

--- config.js
+++ config.js
@@ -2,5 +2,8 @@ require('dotenv').config()
 
 module.exports = {
   telegramToken: process.env.TELEGRAM_TOKEN,
-  mongodbUri: process.env.MONGODB_URI
+  mongodbUri: process.env.MONGODB_URI,
+  startDate: process.env.COMPETITION_START_DATE,
+  endDate: process.env.COMPETITION_END_DATE,
+  duration: process.env.COMPETITION_DURATION
 }
