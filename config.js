4cd536c440950583fc852cd4868fee7003ce627b
arttukauppinen <kauppinen27@gmail.com>
2024-03-14 12:00:28 +0200

    tz adjustment

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 const allowedDates = process.env.ALLOWED_DATES ? process.env.ALLOWED_DATES.split(',') : []
-//const timezone = 'Europe/Helsinki'
 
 module.exports = {
   telegramToken: process.env.TELEGRAM_TOKEN,
@@ -9,7 +8,7 @@ module.exports = {
   startDate: process.env.COMPETITION_START_DATE,
   endDate: process.env.COMPETITION_END_DATE,
   duration: process.env.COMPETITION_DURATION,
-  //reminderTime: process.env.REMINDER_TIME,
+  // reminderTime: process.env.REMINDER_TIME,
+  reminderTime: '12:07',
   allowedDates,
-  //timezone
 }
--- utils/schedule-reminders.js
+++ utils/schedule-reminders.js
@@ -4,17 +4,26 @@ const { sendReminder } = require('../services/user-service')
 const config = require('../config')
 
 const scheduleReminders = () => {
-  console.log('Scheduled reminder time:', config.reminderTime, 'Timezone:', config.reminderTimezone)
   try {
-    schedule.scheduleJob('20 11 * * 4', function(){
-      console.log('Reminder is being executed.')
-      console.log(bot)
-      console.log(bot.telegram)
-      const currentDate = new Date().toISOString().split('T')[0]
-      if (config.allowedDates.includes(currentDate)) {
+    const timeZoneAdj = -2
+    const timeParts = config.reminderTime.split(':')
+    const hour = parseInt(timeParts[0], 10) + timeZoneAdj
+    const minute = parseInt(timeParts[1], 10)
+
+    config.allowedDates.forEach((date) => {
+      const dateParts = date.split('-')
+      const month = parseInt(dateParts[1], 10)
+      const day = parseInt(dateParts[2], 10)
+      console.log(hour, minute, month, day)
+      const cronString = `${minute} ${hour} ${day} ${month} *`
+      schedule.scheduleJob(cronString, function() {
+        console.log('Reminder is being executed.')
+        console.log(bot)
+        console.log(bot.telegram)
         sendReminder(bot)
-      }
+      })
     })
+
   } catch (error) {
     console.error('Scheduling error:', error)
   }
