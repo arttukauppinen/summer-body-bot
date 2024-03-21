9b88d732bd79768d7f5303d7035d16d240b31fbf
arttukauppinen <kauppinen27@gmail.com>
2024-03-21 10:15:30 +0200

    activated the bot

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ const { guildStandingsScene } = require('./flows/statistics-flows/guild-standing
 const { guildComparisonScene } = require('./flows/statistics-flows/guild-comparison')
 
 const onlyPrivate = require('./utils/check-private')
-const isCompetitionActive = require('./utils/is-comp-active')
+//const isCompetitionActive = require('./utils/is-comp-active')
 
 const texts = require('./utils/texts')
 
@@ -61,23 +61,23 @@ bot.use(stage.middleware())
 //   }
 // })
 
-bot.command('start', isCompetitionActive, onlyPrivate, (ctx) => { ctx.scene.enter('start_wizard') })
-bot.command('howtogetpoints', isCompetitionActive, onlyPrivate, (ctx) => { ctx.scene.enter('how_to_get_points_scene') })
-bot.command('statsinfo', isCompetitionActive, onlyPrivate, (ctx) => { ctx.scene.enter('stats_info_scene') })
-bot.command('help', isCompetitionActive, (ctx) => { ctx.scene.enter('help_scene') })
-bot.command('terms', isCompetitionActive, onlyPrivate, (ctx) => { ctx.scene.enter('terms_scene') })
+bot.command('start', onlyPrivate, (ctx) => { ctx.scene.enter('start_wizard') })
+bot.command('howtogetpoints', onlyPrivate, (ctx) => { ctx.scene.enter('how_to_get_points_scene') })
+bot.command('statsinfo', onlyPrivate, (ctx) => { ctx.scene.enter('stats_info_scene') })
+bot.command('help', (ctx) => { ctx.scene.enter('help_scene') })
+bot.command('terms', onlyPrivate, (ctx) => { ctx.scene.enter('terms_scene') })
 
-bot.command('rmuser', isCompetitionActive, onlyPrivate, (ctx) => { ctx.scene.enter('delete_user_wizard') })
-bot.command('register', isCompetitionActive, onlyPrivate, (ctx) => { ctx.scene.enter('register_wizard') })
-bot.command('createteam', isCompetitionActive, onlyPrivate, (ctx) => { ctx.scene.enter('create_team_wizard') })
-bot.command('jointeam', isCompetitionActive, onlyPrivate, (ctx) => { ctx.scene.enter('join_team_wizard') })
-bot.command('weekscores', isCompetitionActive, onlyPrivate, (ctx) => { ctx.scene.enter('week_scores_wizard') })
+bot.command('rmuser', onlyPrivate, (ctx) => { ctx.scene.enter('delete_user_wizard') })
+bot.command('register', onlyPrivate, (ctx) => { ctx.scene.enter('register_wizard') })
+bot.command('createteam', onlyPrivate, (ctx) => { ctx.scene.enter('create_team_wizard') })
+bot.command('jointeam', onlyPrivate, (ctx) => { ctx.scene.enter('join_team_wizard') })
+bot.command('weekscores', onlyPrivate, (ctx) => { ctx.scene.enter('week_scores_wizard') })
 
-bot.command('leaderboards', isCompetitionActive, (ctx) => { ctx.scene.enter('team_rankings_scene') })
-bot.command('team', isCompetitionActive, (ctx) => { ctx.scene.enter('team_member_rankings_scene') })
-bot.command('summary', isCompetitionActive, (ctx) => { ctx.scene.enter('user_summary_scene') })
-bot.command('tikvspt', isCompetitionActive, (ctx) => { ctx.scene.enter('guild_standings_scene') })
-bot.command('tikvsptall', isCompetitionActive, (ctx) => { ctx.scene.enter('guild_comparison_scene') })
+bot.command('leaderboards', (ctx) => { ctx.scene.enter('team_rankings_scene') })
+bot.command('team', (ctx) => { ctx.scene.enter('team_member_rankings_scene') })
+bot.command('summary', (ctx) => { ctx.scene.enter('user_summary_scene') })
+bot.command('tikvspt', (ctx) => { ctx.scene.enter('guild_standings_scene') })
+bot.command('tikvsptall', (ctx) => { ctx.scene.enter('guild_comparison_scene') })
 
 bot.command('dumbahhbot', (ctx) => { ctx.reply('yo mama')})
 
